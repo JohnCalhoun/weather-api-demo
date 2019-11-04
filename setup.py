862c4df4c4b38485a7961f8f2610848bd2863238
John M Calhoun <johnmcalhoun123@gmail.com>
2019-11-03 21:08:48 -0600

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "weather",
+    version = "1.0.0",
+    author = "John Calhoun",
+    author_email = "johnmcalhoun123@gmail.com",
+    description = ("A Demo of creating a OpenWeather python API."),
+    license = "BSD",
+    keywords = "OpenWeather",
+    url = "https://github.com/JohnCalhoun/weather-api-demo",
+    packages=['requests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
